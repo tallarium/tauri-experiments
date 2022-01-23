18a32989cafca8f206e0812cd4fef9b72b2e905d
Adrian Williams <adrian@tallarium.com>
2022-01-23 20:27:57 +0000

    app.listen_global events don't fire

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,17 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+      .setup(|app| {
+
+        app.listen_global("tauri://move", |_event| {
+          println!("MOVE!");
+        });
+        Ok(())
+      })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
